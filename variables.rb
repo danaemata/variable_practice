b36241fd0726887d7771b46389fd8052e0456db7
Danae Mata <danae.mata1515@gmail.com>
2022-11-01 20:31:53 -0400

    Add variables and put statements in file

--- variables.rb
+++ variables.rb
@@ -1,8 +1,19 @@
 user_name = "danaem"
-color = "purple"
+user_name # "kevinc"
+
+favorite_color = "purple"
+favorite_color # "green"
+
 favorite_pet = "lila"
+favorite_pet # "Dino"
+
 favorite_number = "10"
+
 sleep_time = "8"
+puts sleep_time
+
 chipotle_price = "13.43"
+puts chipotle_price
+
 ugly_dog = false
 scary_tree = true
